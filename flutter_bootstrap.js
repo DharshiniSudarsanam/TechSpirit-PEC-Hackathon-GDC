043e891a4fa7785c0f2bbb6e05d9e478e87dd4ef
Dharshini <dharshinisudarsanam@gmail.com>
2025-12-31 00:00:24 +0530

    Deploy updated Flutter web app to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3745312990"
+    serviceWorkerVersion: "741637504"
   }
 });
